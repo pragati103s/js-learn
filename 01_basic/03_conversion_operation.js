1389a8e1e8ac0df2579de79699188b6c1f2e9122
pragati103s <48921897+pragati103s@users.noreply.github.com>
2025-01-28 10:11:41 +0000

    conversion

---
+++ 01_basic/03_conversion_operation.js
@@ -0,0 +1,27 @@
+let score = 33
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// let score2 = "33"
+let score2 = "hello"
+// console.log(typeof(score2))
+let valueInNumber = Number(score2)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true : 1 / false: 0
+
+let isLogged = 1
+let boolIsLogged = Boolean(isLogged)
+console.log(boolIsLogged)
+
+// 1: true ; 0: false
+//  " ": false
+// "num" : true
+
+let someNumber = 33
+let stringNUmber = String( someNumber)
+console.log(stringNUmber)
+console.log(typeof stringNUmber)
