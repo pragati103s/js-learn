3a90ea583860d4f8d34faaf96d19fd88686c5d94
pragati103s <48921897+pragati103s@users.noreply.github.com>
2025-01-28 09:41:25 +0000

    variables

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,24 @@
+// "use strict"; treate all js code as a new version
+
+// alert()   // use it in browser not in nodejs
+console.log(3+3)
+
+let name = "pragati"
+let age = 1
+let isLoggedIn = false
+let state  // when it will console it gives undefined but
+let st = null // it gived null
+
+// number => 2 to power 53
+// bigint
+//  string => " "
+// boolean => true/false
+// null : it is datatype and also standalone value and also an object
+// undefined :   it means value is not assigned
+// symbole : use to find uniqueness in module
+
+
+// object
+
+console.log(typeof "hello")
+console.table([ typeof age, typeof null, typeof undefined])
