dd3fbb115295a114bd5660326afcb80498c8e96e
pragati103s <48921897+pragati103s@users.noreply.github.com>
2025-01-28 07:50:38 +0000

    learn varibles in js

---
+++ 01_basic/01_varibale.js
@@ -0,0 +1,17 @@
+// two types of variable:
+/* 
+    1. const : not modifiying 
+    2. variable - let, var
+    let- change their value inside the scope
+    var- change their value in whole program
+*/
+// declaration 
+const name = " world"
+let email = "test@gmail.com"
+var mobileno = 1234567890
+
+let accountState
+
+
+console.table([name, email, mobileno,accountState])
+console.log(name, email, mobileno)
