f3f4b5345d71e047c2f09f60d397b927c94a7285
pragati103s <48921897+pragati103s@users.noreply.github.com>
2025-01-29 08:46:21 +0000

    nums&maths

---
+++ 01_basic/06_nums&maths.js
@@ -0,0 +1,33 @@
+const score = 400
+// console.log(typeof(score) );
+
+const balance = new Number(100)
+// console.log((balance));
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2))  // it is used to fix some digit after decimal (precision value)
+const otherNumber = 23.8956
+
+// console.log(otherNumber.toPrecision(3)); // it prioritise before decimal, use carefully
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  there is osme more Number function in JS which can be get in console 
+// just type Number. 
+//  to be practised
+
+// ++++++++++++++++++++++++++++++++++++++++ MAths +++++++++++++++++++++++++++
+console.log(Math)
+
+console.log(Math.abs(-435));
+console.log((Math.round(4.6)));  //ceil and floor
+
+// min and max
+console.log(Math.random())  // values between 0 and 1
+ 
+
+
+
+
