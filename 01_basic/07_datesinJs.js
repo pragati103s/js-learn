87206a7c9dccac96b009091fe76073c037d21f2e
pragati103s <48921897+pragati103s@users.noreply.github.com>
2025-01-31 05:34:41 +0000

    dates

---
+++ 01_basic/07_datesinJs.js
@@ -0,0 +1,16 @@
+let myDate= new Date()
+
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toUTCString());
+
+// let myCreatedDate = new Date(2023,0,25)
+// let myCreatedDate = new Date(2023,0,25,5,3)
+let myCreatedDate = new Date("01-02-2025")
+console.log(myCreatedDate.toLocaleString());
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
